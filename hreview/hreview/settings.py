849cca2d538313dbab1ccb776a7acee49dbc3fe0
EunjiShin <simsimhae_98@naver.com>
2020-05-19 00:33:05 +0900

    add static and rewrite store.html

--- hreview/hreview/settings.py
+++ hreview/hreview/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = "main"
 LOGOUT_REDIRECT_URL = "main"
