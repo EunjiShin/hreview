f061b579286b7cccc6a256e9baeeb363520e6f45
jyoungl <pmis118@naver.com>
2020-06-07 23:44:05 +0900

    modify mapage and add navbar, footer

--- hreview/hyuzi/context_processors.py
+++ hreview/hyuzi/context_processors.py
@@ -1,5 +1,7 @@
 from .models import Cart, CartItem
 from .views import _cart_id
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def counter(request):
     item_count = 0
@@ -11,6 +13,6 @@ def counter(request):
             cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 item_count += cart_item.quantity
-        except Cart.DoseNotExist:
+        except ObjectDoesNotExist:
             item_count = 0
     return dict(item_count = item_count)
